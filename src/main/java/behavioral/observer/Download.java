bc9230afe7deb66547d63291eb5a2c9fb0ae33df
txemasv <txemasv@gmail.com>
2018-03-15 17:15:09 +1100

    Observer

--- src/main/java/behavioral/observer/Download.java
+++ src/main/java/behavioral/observer/Download.java
@@ -1,11 +1,17 @@
 package behavioral.observer;
 
-import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.ExecutorService;
 
 public class Download implements Observable {
 
-    private List<Observer> observers = new ArrayList<>();
+    private List<Observer> observers;
+    private ExecutorService executor;
+
+    public Download(List<Observer> observers, ExecutorService executor) {
+        this.executor = executor;
+        this.observers = observers;
+    }
 
     @Override
     public void subscribe(Observer observer) {
@@ -26,8 +32,13 @@ public class Download implements Observable {
     }
 
     public void downloadAsset(String uri) {
-        System.out.println("Downloading asset '" + uri + "'...");
-        //when asset download is completed:
-        notifyObserver();
+        System.out.println("Download asset '" + uri);
+        Runnable run = () -> {
+            System.out.println("Downloading ...");
+            //do stuff to download file,
+            //when asset download is completed
+            notifyObserver();
+        };
+        executor.submit(run);
     }
 }
--- src/test/java/behavioral/observer/ObserverTest.java
+++ src/test/java/behavioral/observer/ObserverTest.java
@@ -3,20 +3,30 @@ package behavioral.observer;
 import org.junit.Before;
 import org.junit.Test;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+
 import static org.junit.Assert.assertEquals;
 
 public class ObserverTest {
 
     private Log log;
+    private ExecutorService executorService;
+    private List<Observer> observers;
 
     @Before
     public void setUp() {
         log = new Log();
+        executorService = Executors.newSingleThreadExecutor();
+        observers = new ArrayList<>();
     }
 
     @Test
-    public void download_Should_Notify_UserInterface_When_DownloadAsset_finish() {
-        Download download = new Download();
+    public void download_Should_Notify_UserInterface_When_DownloadAsset_IsCompleted() {
+        Download download = new Download(observers, executorService);
         UserInterface userInterface = new UserInterface(log);
 
         download.subscribe(userInterface);
@@ -24,6 +34,17 @@ public class ObserverTest {
         String assetUri = "https://example.org/resource.txt";
         download.downloadAsset(assetUri);
 
+        executorServiceJoinAll();
+
         assertEquals("Download finished!", log.getMessage());
+    }
+
+    private void executorServiceJoinAll() {
+        executorService.shutdown();
+        try {
+            executorService.awaitTermination(Long.MAX_VALUE, TimeUnit.NANOSECONDS);
+        } catch (InterruptedException e) {
+            System.out.println(e.getMessage());
+        }
     }
 }
