b5cc5c845e7d78f8239b357d1d51e7f02bb8c54e
txemasv <txemasv@gmail.com>
2018-03-16 10:37:17 +1100

    Memento

--- src/test/java/behavioral/memento/MementoTest.java
+++ src/test/java/behavioral/memento/MementoTest.java
@@ -1,31 +1,40 @@
 package behavioral.memento;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
 
 public class MementoTest {
 
+    private int current;
+
+    @Before
+    public void setUp() {
+        current = -1;
+    }
+
     @Test
     public void restoreMemento_Should_Return_BackState_Of_Memory() {
         CareTaker careTaker = new CareTaker();
         Originator originator = new Originator();
-        int current = 0;
 
         Memory myMemory1 = new Memory("6th anniversary", "I had a party with my friends and they offer me 'The Steadfast Tin Soldier' book.");
         originator.setMemory(myMemory1);
         careTaker.addMemento(originator.storeInMemento());
+        current++;
 
         Memory myMemory2 = new Memory("first kiss", "When I was 11 a girl of my school kiss me, I felt so happy.");
         originator.setMemory(myMemory2);
         careTaker.addMemento(originator.storeInMemento());
+        current++;
 
-        Memory savedMemory = originator.restoreFromMemento(careTaker.getMemento(undo(current)));
+        Memory savedMemory = originator.restoreFromMemento(careTaker.getMemento(undo()));
 
         assertEquals(myMemory1, savedMemory);
     }
 
-    private int undo(int current) {
+    private int undo() {
         if (current > 0) current--;
         return current;
     }
